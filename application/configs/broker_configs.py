54bea87043fdbb7283024426e1e16b8ea91a97e9
Rafaelrdl <rafaelrdlessa@gmail.com>
2024-06-12 14:10:49 -0300

    Implementação do arquivo main e correções

--- application/configs/broker_configs.py
+++ application/configs/broker_configs.py
@@ -1,6 +1,6 @@
 mqtt_broker_configs ={
     "HOST": "mqtt.tago.io",
     "PORT": 1883,
-    "CLIENT_NAME": "teste-paho"
-    "KEPPALIVE": 60
+    "CLIENT_NAME": "teste-paho",
+    "KEPPALIVE": 60,
 }
